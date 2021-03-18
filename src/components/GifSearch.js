88895a99382dacc34f8cc1741095914fd1fd9182
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2021-03-18 21:37:35 +0000

    Automatically backed up by Learn

---
+++ src/components/GifSearch.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+
+class GiftSearch extends React.component{
+  constructor(props){
+    super()
+   
+  }
+  
+  render(){
+   
+    return(
+      <div>
+    
+      </div>
+      )
+  }
+}
+
+export default GiftSearch
