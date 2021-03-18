b6169320ad9089f92fcd2aa87114d16c729d3212
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2021-03-18 21:32:43 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -14,10 +14,16 @@ class GiftListContainer extends React.component{
   render(){
     componenDidMount(){
       const fetchTheGifs=()=>{
-        
+         fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`)
+      .then(res => res.json())
+      .then(({data}) => {
+        this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+      })
       }
     }
     return(
-      <div></div>)
+      <div>
+      <GifSearch />
+      </div>)
   }
 }
