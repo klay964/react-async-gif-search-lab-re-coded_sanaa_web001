898a2b527c49cbe3437d09fc01a51d971a0413d5
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2021-03-18 21:29:59 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -3,4 +3,11 @@ import GifList from '../components/GifList'
 import GifSearch from '../components/GifSearch'
 
 
-class GiftListContainer extends React.component
+class GiftListContainer extends React.component{
+  constructor(){
+    super()
+    this.state={
+      
+    }
+  }
+}
