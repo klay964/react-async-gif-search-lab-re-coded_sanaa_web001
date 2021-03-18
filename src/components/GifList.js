b9dc4ff967d4e5b9282c5bd92866a2a8627ba2c8
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2021-03-18 21:35:08 +0000

    Automatically backed up by Learn

---
+++ src/components/GifList.js
@@ -0,0 +1,23 @@
+import React from "react"
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
+
+
+class GiftList extends React.component{
+  constructor(){
+    super()
+    this.state={
+      gifs:[]
+    }
+  }
+  
+  render(){
+   
+    return(
+      <div>
+     
+      </div>)
+  }
+}
+
+export default GiftList
